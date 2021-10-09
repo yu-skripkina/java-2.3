a7196d6524d8b6c92ef64a69c511e102746f2277
Yuliya Skripkina <yu.skripkina@gmail.com>
2021-10-09 23:29:41 +0300

    Added new tests

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -16,7 +16,7 @@ class BonusServiceTest {
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-       assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -26,6 +26,63 @@ class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000_000_60;
         boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndBonusIsEqualLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredAndBonusIsEqualLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForUnRegisteredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_000_60;
+        boolean registered = false;
         long expected = 500;
 
         // вызываем целевой метод:
